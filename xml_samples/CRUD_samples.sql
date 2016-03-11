133753575167b7893b1f9ad7a5a08b1518c0f236
Amanda Gilmore <ms.amgilmore@gmail.com>
2016-03-11 13:57:33 -0800

    first commit, XML syntax

---
+++ xml_samples/CRUD_samples.sql
@@ -0,0 +1,38 @@
+INSERT INTO xml_sample (first_column, second_column) values (
+XMLPARSE( DOCUMENT $$<?xml version="1.0"?>
+<catalog>
+...
+</catalog>$$)
+, 'This is an entire XML document')
+
+--alternatively...
+
+INSERT INTO xml_sample (first_column, second_column) values (
+$$<book id="bk101">
+      <author>Gambardella, Matthew</author>
+      <title>XML Developer\'s Guide</title>
+      <genre>Computer</genre>
+      <price>44.95</price>
+      <publish_date>2000-10-01</publish_date>
+      <description>An in-depth look at creating applications
+      with XML.</description>
+   </book>$$
+   , 'one book per row, as an XML node')
+
+
+
+
+
+--Gets all titles in the table
+
+SELECT xpath('//book/title', first_column) from xml_sample;
+
+--Gets all info on a book by title
+
+SELECT * FROM xml_sample WHERE (xpath('//book/title/text()'
+   , first_column))[1]::text = 'Midnight Rain'::text
+
+
+
+
+
