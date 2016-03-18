73581667fd343a8322ea3819e448d51e09d8e451
Amanda Gilmore <ms.amgilmore@gmail.com>
2016-03-17 17:04:20 -0700

    querying on json path wtf

--- json_samples/json_CRUD_samples.sql
+++ json_samples/json_CRUD_samples.sql
@@ -19,10 +19,21 @@ $$
 
 
 
---Can select at top level:
+--Can select at top level. This SHOULD check if the string is a subset of the json.
+--TODO figure out why this returns no rows
 
-SELECT * FROM json_samples WHERE text_json @> '{ "fpVal": 57.0 }'
-SELECT * FROM json_samples WHERE binary_json @> '{ "fpVal": 57.0 }'
+SELECT * FROM json_samples WHERE text_json::jsonb @> '{ "fpVal": 57.0 }'
+SELECT * FROM json_samples WHERE binary_json @> '{ "fpVal": 57.0 }'::jsonb
 
---Or at a more granular level:
+--Or if a given key is present:
 
+SELECT * FROM json_samples WHERE text_json::jsonb ? 'dataSourceId'
+SELECT * FROM json_samples WHERE binary_json ? 'dataSourceId'
+
+--You can check if the value of the key is something:
+
+SELECT * FROM json_samples
+WHERE binary_json ->> 'minStartTimeNs' = '1429828653617000000'
+
+--TODO cannot figure out how to query on more deeply nested keys,
+--e.g. specififc json path
